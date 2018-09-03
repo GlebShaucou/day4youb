a05541fabeb2037744157338194dd45f333ff5d3
Hleb Shautsou <hleb_shautsou@epam.com>
2018-09-03 11:12:00 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <h1 className="header__title">Day 4 You</h1>
+        </header>
+	      <main>
+
+	      </main>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
