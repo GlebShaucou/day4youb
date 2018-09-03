84f0ec0ea439152db1c90e89480e91ead6760363
Hleb Shautsou <hleb_shautsou@epam.com>
2018-09-03 13:21:56 +0300

    structure added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,83 @@
 import React, { Component } from 'react';
+import data from '../../data-layer/data';
 import './App.css';
 
+const { promoCodes, images } = data;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <header className="app__header">
-          <h1 className="header__title">Day 4 You</h1>
-        </header>
-	      <main>
+	renderArticles() {
+		const imagesForRender = Object.keys(images);
 
-	      </main>
+		return imagesForRender.map((imageName, index) => {
+			const image = images[imageName];
+			const {
+				title,
+				description,
+				src,
+				alt,
+			} = image;
 
+			return (
+				<div key={index} className="article">
+					<img src={src} alt={alt} className="article__image" />
+					<h4 className="article__title">
+						{title}
+					</h4>
+					<div className="article__description">
+						{description}
+					</div>
+				</div>
+			);
+		});
+	}
 
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<header className="app__header">
+					<div className="app-container">
+						<h1 className="header__title">Day 4 You</h1>
+					</div>
+				</header>
+				<main className="app__main">
+					<div className="title-section">
+						<div className="app-container">
+							<h2 className="title-section__header">Day For You</h2>
+						</div>
+					</div>
+					<div className="promo-section">
+						<div className="app-container">
+							<div className="promo-section__form">
+								<label className="promo-label" htmlFor="promo">
+									Enter promo code here:
+								</label>
+								<input
+									id="promo"
+									className="promo-section__input"
+									type="text"
+								/>
+							</div>
+						</div>
+					</div>
+					<div className="project-description-section">
+						<div className="app-container">
+							<div className="description-section">
+								<h3 className="description__title">
+									Day For You application description
+								</h3>
+								<div className="description__body">
+									{this.renderArticles()}
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+				<footer className="app__footer">
+					footer
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/data-layer/data.js
+++ src/data-layer/data.js
@@ -4,6 +4,28 @@ const poromoCodes = {
     UYRETD: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 };
 
-export {
+const images = {
+	GUITAR: {
+		title: 'Lorem ipsum',
+		description: 'lorem dolor sit amet, consectetur adipiscing elit,',
+		src: 'http://i68.tinypic.com/zm1es0.jpg',
+		alt: 'guitar',
+	},
+	POTTERY: {
+		title: 'Lorem ipsum',
+		description: 'lorem dolor sit amet, consectetur adipiscing elit,',
+		src: 'http://i67.tinypic.com/2ro4zn7.jpg',
+		alt: 'pottery',
+	},
+	WEDDING: {
+		title: 'Lorem ipsum',
+		description: 'lorem dolor sit amet, consectetur adipiscing elit,',
+		src: 'http://i68.tinypic.com/2hmcymd.jpg',
+		alt: 'wedding',
+	},
+};
+
+export default {
     poromoCodes,
+	images,
 };
