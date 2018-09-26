b2bc8a8b2da8377fb92c4aade202f038d748c168
Gleb Shaucou <gleb.shaucou@gmail.com>
2018-09-27 00:01:03 +0300

    major changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import data from '../../data-layer/data';
+
+import fonImgSrc from './img/fon.jpg';
+import cafeImgSrc from './img/cafe.jpg';
+import closedPromoImgSrc from './img/closed-promo.jpg';
+import questionImgSrc from './img/question.jpg';
+import starsImgSrc from './img/stars.jpg';
+import teaImgSrc from './img/tea.jpg';
+import vilageImgSrc from './img/village.png';
+
 import './App.css';
 
 const { defaultPromo } = data;
 let promoCodesFromData = data.promoCodes;
 const localStorage = window.localStorage;
 const PROMO_CODES_LOCAL_STORAGE = 'promoCodes';
+
+const imageUrlsForPromo = {
+	cafe: cafeImgSrc,
+	closedPromo: closedPromoImgSrc,
+	question: questionImgSrc,
+	stars: starsImgSrc,
+	tea: teaImgSrc,
+	village: vilageImgSrc,
+};
 
 if (localStorage) {
 	try {
@@ -107,30 +125,44 @@ class App extends Component {
 		});
 	};
 
+	openPromoDialog = (promoCode) => () => {
+		this.setState({
+			promoCode,
+			showPromoCodeDialog: true,
+		})
+	}
+
 	renderArticles() {
 		const { promoCodes } = this.state;
 
 		return promoCodes.map((promo, index) => {
 			const { isOpen } = promo;
 			let title;
-			let description;
+			let place;
 			let src;
 			let alt;
+			let code;
+			let time;
 
 			if (isOpen) {
-				({ title, description, src, alt } = promo);
+				({ code, title, time, place, src, alt } = promo);
 			} else {
-				({ title, description, src, alt } = defaultPromo);
+				({ title, place, src, alt } = defaultPromo);
 			}
 
 			return (
-				<div key={index} className="article">
-					<img src={src} alt={alt} className="article__image" />
+				<div key={index} className="article" onClick={isOpen ? this.openPromoDialog(code) : () => {}}>
+					<img src={imageUrlsForPromo[src]} alt={alt} className="article__image" />
 					<h4 className="article__title">
 						{title}
 					</h4>
 					<div className="article__description">
-						{description}
+						<div className="dialog__time">
+							{time}
+						</div>
+						<div>
+							{place}
+						</div>
 					</div>
 				</div>
 			);
@@ -204,7 +236,7 @@ class App extends Component {
 		)
 	}
 
-	renderPromoCodeDialog() {
+	renderPromoCodeDialog({ promo } = {}) {
 		const {
 			promoCode,
 			promoCodes,
@@ -220,15 +252,18 @@ class App extends Component {
 		let description = 'Вы ввели неверный промо-код. Проверьте введенные и попробуйте еще раз.';
 		let src = defaultPromo.src;
 		let alt = '';
+		let time = '';
+		let place = '';
+		let placeUrl = '';
 
 		if (selectedPromo) {
-			({ title, description, src, alt } = selectedPromo)
+			({ title, time, place, placeUrl, description, src, alt } = selectedPromo)
 		}
 
 		return (
 			<div className="modal-dialog">
-				<div className="modal-overlay" />
-				<div className="dialog-overlay">
+				<div className="modal-overlay"/>
+				<div className="dialog-overlay" >
 					<div className="dialog">
 						<div className="dialog__header">
 							<span className="dialog__header-text">
@@ -238,11 +273,19 @@ class App extends Component {
 						<div className="dialog__body">
 							<div className="dialog__body-description">
 								<div className="dialog__image-container">
-									<img src={src} alt={alt} className="dialog__image" />
+									<img src={imageUrlsForPromo[src]} alt={alt} className="dialog__image" />
 								</div>
-								<p className="dialog__text">
-									{description}
-								</p>
+								<div className="dialog__text">
+									<div className="dialog__time">
+										{time}
+									</div>
+									<div className="dialog__place" onClick={() => { window.open(placeUrl, '_blank'); }}>
+										{place}
+									</div>
+									<div className="dialog__description">
+										{description}
+									</div>
+								</div>
 							</div>
 						</div>
 						<span
@@ -266,9 +309,35 @@ class App extends Component {
 					{this.renderNavigationSection()}
 				</header>
 				<main className="app__main">
-					<div className="title-section" id="home">
+					<div className="title-section" style={{ backgroundImage: `url(${fonImgSrc})` }} id="home">
 						<div className="app-container">
 							<h2 className="title-section__header">Day For You</h2>
+						</div>
+					</div>
+					<div className="congrats-section">
+						<div className="app-container">
+							<h2 className="congrats-section__title">
+								Поздравляем, ваш день 28.09.2018г.! 
+							</h2>
+							<div className="congrats-section__description">
+								Вы стали победителем рекламной игры  от сети наших партнёров. От Вас требуется только наличие свободного времени и желание хорошо провести  день. 
+								Промо-коды буду действительны только в течение одного дня 28 сентября 2018г. Все наши мероприятия являются для Вас абсолютно бесплатными и доступны к посещению компанией  до 2-х человек. 
+								<br />
+								Правила использования промо-кодов:
+								<ol className="steps-list">
+									<li>
+										Активируйте полученный промо-код на сайте day4you.by.
+									</li>
+									<li>
+										Предоставить карточку с промо-кодом в месте проведения мероприятия. 
+									</li>
+									<li>
+										После каждого посещенного мероприятия, Вы получаете карту для активации последующего промо-кода. 
+									</li>
+								</ol>
+
+								Желаем Вам хорошего дня)
+							</div>
 						</div>
 					</div>
 					<div className="promo-section" id="promo">
@@ -310,7 +379,20 @@ class App extends Component {
 				</main>
 				<footer className="app__footer" id="contacts">
 					<div className="app-container">
-						footer
+						<div className="footer__text">
+						Ваша жизнь — это комплекс действий, решений и идей. Опыт, который вы получаете на протяжении своей жизни, зависит только от того, как вы выстраиваете свой день. Любые события способны превратить вашу жизнь в прекрасный калейдоскоп событий. Даже самые незначительные решения помогут вам достичь самой далёкой цели. 
+						<br />
+						<br />
+						администрация day4you.by 
+						<br />
+						<br />
+							<div className="footer__email">
+								E-mail: <a href="mailto:280918@day4you.by">280918@day4you.by</a> 
+							</div>
+							<div className="web-development">
+								Веб разработка <a href="https://www.linkedin.com/in/hleb-shautsou-1726ba109/" target="_blank">Lamsbeg Group</a>
+							</div>
+						</div>
 					</div>
 				</footer>
 				{this.renderPromoCodeDialog()}
